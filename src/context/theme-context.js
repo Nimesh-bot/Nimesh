6b1c35be1ec9022587d80d95f19183e727240f7b
Nimesh Shakya <somit409@gmail.com>
2022-08-08 13:18:11 +0545

    changed Wallpaper

--- src/context/theme-context.js
+++ src/context/theme-context.js
@@ -1,13 +1,13 @@
 import { createContext, useState } from "react"
 import { themes } from "./Themes";
-import { BandW, Snooke } from "../components";
+import { LightMe } from "../components";
 
 export const ThemeContext = createContext();
 
 const ThemeProvider = ({ children }) => {
     const [theme, setTheme] = useState(themes.heal);
     
-    const [wallpaper, setWallpaper] = useState(BandW);
+    const [wallpaper, setWallpaper] = useState(LightMe);
 
     const [viewerDetails, setViewerDetails] = useState({
         name: '',
